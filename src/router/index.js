dc1853c8b5cdc905b55d8cfcb46e7fda4c30bcfa
Guo <gty714904821@gmail.com>
2024-11-12 12:58:51 +0800

    打包本地预览

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory,createWebHashHistory  } from 'vue-router'
 import HomeView from '../views/elder/elders.vue'
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHashHistory (import.meta.env.BASE_URL),
 	routes: [
 		{
 			path: '/',
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/',// 开发或生产环境服务的公共基础路径 配置引入相对路径
+	base: './',// 开发或生产环境服务的公共基础路径 配置引入相对路径
 	server: {
 		host: '0.0.0.0',
 		open: true,
@@ -25,7 +26,13 @@ export default defineConfig({
 			}
 		}
 	},
-	plugins: [vue()],
+// 	vite构建打包后，默认启用ES Module，跨module加载引用涉及跨域，file://…没有跨域的定义
+// 需要安装vite插件@vitejs/plugin-legacy 需在node 22.10.0 版本上打包 不然报错
+	plugins: [vue(),
+		legacy({
+			targets:['defaults','not IE 11']
+		})
+	],
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
